3c50b8148de4731c5852ab3175adbd675cd67747
Oliver Keyes <ironholds@gmail.com>
2016-12-27 16:19:23 -0800

    simplify and genericise code

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,73 +1,65 @@
 #include "parser.h"
 
-void wkt_parser::point_parser::validate_wkt(std::string& wkt, String& rationale, bool result){
-
-  // Strip braces
-  if(wkt[0] != '(' | wkt[wkt.size()-1] != ')'){
-    result = false;
-    rationale = "WKT objects are expected to have braces around the set of coordinates";
-  } else {
-    wkt.erase(0,1);
-    wkt.erase(wkt.size()-1, 1);
-
-    std::deque < std::string > holding;
-    wkt_utils::split_elements(wkt, holding, " ");
-    Rcpp::print(Rcpp::wrap(holding[0]));
-    Rcpp::print(Rcpp::wrap(holding[1]));
-
-    if(holding.size() == 2){
-      if(wkt_utils::check_digit(holding[0]) && wkt_utils::check_digit(holding[1])){
-        result = true;
-        rationale = NA_STRING;
-      } else {
-        result = false;
-        rationale = "Coordinates must be numeric values";
-      }
-    } else {
+void wkt_parser::check_pointpair(std::string& pointpair, String& rationale,
+                                 bool& result, bool& alter){
+  std::deque < std::string > holding;
+  wkt_utils::split_elements(pointpair, holding, " ");
+  if(holding.size() == 2){
+    if(wkt_utils::check_digit(holding[0]) && wkt_utils::check_digit(holding[1])){
       result = false;
-      rationale = "Point WKT objects should have two, space-separated coordinates";
+      alter = true;
+      rationale = "Coordinates must be numeric values";
+    }
+  } else {
+    result = false;
+    alter = true;
+    rationale = "WKT object coordinates should come in pairs";
+  }
+}
+
+void wkt_parser::check_braces(std::string& wkt_obj, String& rationale, bool& result,
+                              bool& alter){
+  if(wkt_obj[0] != '(' | wkt_obj[wkt_obj.size()-1] != ')'){
+    result = false;
+    rationale = "WKT objects are expected to have braces around the set(s) of coordinates";
+    alter = true;
+  } else {
+    wkt_obj.erase(0,1);
+    wkt_obj.erase(wkt_obj.size()-1, 1);
+  }
+}
+
+void wkt_parser::point_parser::validate_wkt(std::string& wkt, String& rationale, bool& result){
+
+  bool alter = false;
+  wkt_parser::check_braces(wkt, rationale, result, alter);
+  if(!alter){
+    wkt_parser::check_pointpair(wkt, rationale, result, alter);
+    if(!alter){
+      result = true;
+      rationale = NA_STRING;
     }
   }
 }
 
-void wkt_parser::linestring_parser::validate_wkt(std::string& wkt, String& rationale, bool result){
+void wkt_parser::linestring_parser::validate_wkt(std::string& wkt,
+                                                 String& rationale, bool& result){
 
-  bool fail = false;
-  // Strip braces
-  if(wkt[0] != '(' | wkt[wkt.size()-1] != ')'){
-    result = false;
-    fail = true;
-    rationale = "WKT objects are expected to have braces around the set of coordinates";
-  } else {
-    wkt.erase(0,1);
-    wkt.erase(wkt.size()-1, 1);
-
+  bool alter = false;
+  wkt_parser::check_braces(wkt, rationale, result, alter);
+  if(!alter){
     std::deque < std::string > holding;
-    std::deque < std::string > in_holding;
     wkt_utils::split_elements(wkt, holding, ",");
-
     for(unsigned int i = 0; i < holding.size(); i++){
       wkt_utils::clean_wkt(holding[i]);
-      wkt_utils::split_elements(holding[i], in_holding, " ");
-      if(in_holding.size() == 2){
-        if(!wkt_utils::check_digit(in_holding[0]) || !wkt_utils::check_digit(in_holding[1])){
-          result = false;
-          fail = true;
-          rationale = "Coordinates must be numeric values";
-          break;
-        }
-      } else {
-        result = false;
-        fail = true;
-        rationale = "Each point in a LineString should have two, space-separated coordinates." +
-          (std::string)"point " + wkt_utils::to_string(i) + " does not";
+      wkt_parser::check_pointpair(holding[i], rationale, result, alter);
+      if(alter){
         break;
       }
-      in_holding.clear();
     }
-  }
-  if(!fail){
-    result = true;
-    rationale = NA_STRING;
+    if(!alter){
+      result = true;
+      rationale = NA_STRING;
+    }
   }
 }
--- src/parser.h
+++ src/parser.h
@@ -6,13 +6,18 @@ using namespace Rcpp;
 #define __WKT_PARSER__
 namespace wkt_parser {
 
+  //This checks data formatted as "12 45" for, you know, size and suchlike
+  void check_pointpair(std::string& pointpair, String& rationale, bool& result, bool& alter);
+
+  void check_braces(std::string& wkt_obj, String& rationale, bool& result, bool& alter);
+
   namespace point_parser {
 
     //A point object should consist of:
     //* Two coordinates
     //* Stored as numeric values
     //* Separated by a space
-    void validate_wkt(std::string& wkt, String& rationale, bool result);
+    void validate_wkt(std::string& wkt, String& rationale, bool& result);
   };
 
   namespace linestring_parser {
@@ -21,7 +26,7 @@ namespace wkt_parser {
     //* Stored as numeric values
     //* Separated into pairs by a comma
     //* Separated by a space.
-    void validate_wkt(std::string& wkt, String& rationale, bool result);
+    void validate_wkt(std::string& wkt, String& rationale, bool& result);
 
   };
 
